4bb9a80cc8b541b88b9d80cd82f4949f6f06c8b8
unknown <sameerraza13579@gmail.com>
2021-02-17 04:14:41 +0500

    Incomplete

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route path="/about"><About /></Route>
+          <Route path="/login"><Login /></Route>
+          <Route path="/signup"><Signup /></Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
